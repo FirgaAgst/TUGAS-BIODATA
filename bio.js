86ee628b8c14630035f275bec1b520437ce52010
FirgaAgst <161696475+FirgaAgst@users.noreply.github.com>
2024-09-21 17:55:58 +0700

    Add files via upload

---
+++ bio.js
@@ -0,0 +1,14 @@
+const nama = document.querySelector('#Nama');
+nama.textContent = 'Firga Agustian Putra Budiman';
+
+const TTL = document.querySelector('#TTL');
+TTL.textContent = 'Sukabumi,13 agustus 2004';
+
+const Alamat = document.querySelector('#Alamat');
+Alamat.textContent = 'Sukabumi, jl.siliwangi';
+
+const Status = document.querySelector('#Status');
+Status.textContent = 'Mahasiswa';
+
+const email = document.querySelector('#Email');
+email.textContent = 'firgaagus@gmail.com';
